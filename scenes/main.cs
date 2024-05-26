e8f9af8bf39249304d2b8a40c61534a3788490da
Karel Šohaj <sohaj@cross.cz>
2024-05-26 13:15:07 +0200

    Add snake head scene with movement control

---
+++ scenes/main.cs
@@ -0,0 +1,57 @@
+using Godot;
+
+public partial class main : Node2D
+{
+	private readonly double SPEED = 3000.0;
+	private readonly double TIME_BETWEEN_MOVES = 1000.0;
+	private double _timeLastMove = 1000.0;
+
+	private Panel _snakeHead;
+	private Panel _playground;
+	private Vector2 _nextPosition;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		InitElements();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsKeyPressed(Key.Up))
+		{
+			_nextPosition = Vector2.Up * _snakeHead.Size.Y;
+		}
+		else if (Input.IsKeyPressed(Key.Right))
+		{
+			_nextPosition = Vector2.Right * _snakeHead.Size.X;
+		}
+		else if (Input.IsKeyPressed(Key.Down))
+		{
+			_nextPosition = Vector2.Down * _snakeHead.Size.Y;
+		}
+		else if (Input.IsKeyPressed(Key.Left))
+		{
+			_nextPosition = Vector2.Left * _snakeHead.Size.X;
+		}
+
+		_timeLastMove += delta * SPEED;
+		if (_timeLastMove >= TIME_BETWEEN_MOVES)
+		{
+			_snakeHead.Position += _nextPosition;
+
+			_timeLastMove = 0;
+		}
+	}
+
+	private void InitElements()
+	{
+		_snakeHead = GetNode<Panel>("./SnakeHead");
+		_playground = GetNode<Panel>("./Playground");
+
+		_nextPosition = Vector2.Right * _snakeHead.Size.X;
+		_snakeHead.Position = new Vector2(205, 205);
+	}
+}
